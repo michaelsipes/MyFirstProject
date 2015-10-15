692bef01551a61d24522e2e031622110a6aad6f5
Michael <msipes2010@gmail.com>
2015-10-15 13:12:47 -0700

    My first ever GitHub project

---
+++ src/MyFirstPrpoject.java
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 10/15/15.
+ */
+public class MyFirstPrpoject {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+
+}
